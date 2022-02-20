5f32149f38d2ece4549c0d64f83421e602cc1e31
Jonathan Bartlett <jonathan@jonnobrow.co.uk>
2022-02-20 15:43:45 +0000

    feat: Add url encoder and decoder

--- internal/commands/encdec.go
+++ internal/commands/encdec.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"encoding/base64"
+	"net/url"
 )
 
 var (
@@ -12,6 +13,12 @@ var (
 		})
 		converters = append(converters, base64dec{
 			base: NewBase("Base64 Decode", "Decode input from base64"),
+		})
+		converters = append(converters, urlenc{
+			base: NewBase("URL Encode", "Encode input to url"),
+		})
+		converters = append(converters, urldec{
+			base: NewBase("URL Decode", "Decode input from url encoded"),
 		})
 		return converters
 	}
@@ -32,4 +39,20 @@ type base64dec struct {
 func (e base64dec) Exec(raw string) (string, error) {
 	out, err := base64.StdEncoding.DecodeString(raw)
 	return string(out), err
+}
+
+type urlenc struct {
+	base
+}
+
+func (e urlenc) Exec(raw string) (string, error) {
+	return url.PathEscape(raw), nil
+}
+
+type urldec struct {
+	base
+}
+
+func (e urldec) Exec(raw string) (string, error) {
+	return url.PathUnescape(raw)
 }
